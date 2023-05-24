7b21871386ca424ad3bb0ad36bbabd309f29f8c9
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-24 20:08:40 +0300

    implemented displaying the menu in different screens

--- src/app/Components/Header Component/header.component.ts
+++ src/app/Components/Header Component/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  isOpened: boolean = false;
 
+  @HostListener('window: resize')
+  onWindowResize() {
+    this.updateClass();
+  }
+
+  updateClass() {
+    if (window.innerWidth > 900) {
+      this.isOpened = false;
+    }
+  }
+
+  toggleClass() {
+    this.isOpened = !this.isOpened;
+  }
 }

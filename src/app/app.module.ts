3d03f2997095b212e9828bea3fe80b4db02e8051
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-24 16:05:31 +0300

    added the initial structure for the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './Components/App Component/app.component';
 import { HeaderComponent } from './Components/Header Component/header.component';
+import { HomeComponent } from './Components/Home Component/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

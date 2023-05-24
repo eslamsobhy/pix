526e09bfbbb34ee43f33aaa5022b5d3528526a13
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-05-24 14:13:02 +0300

    creating comps and installing bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    AddUserComponent,
   ],
   imports: [
     BrowserModule,

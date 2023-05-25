f25caaf8116263a2288c358af12c90dcdbe47d7e
asmaatajeldein <asmaatajeldein@gmail.com>
2023-05-25 16:28:55 +0300

    get album photos

--- src/app/Components/album-photos/album-photos.component.ts
+++ src/app/Components/album-photos/album-photos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlbumsService } from 'src/app/services/albums.service';
 
 @Component({
   selector: 'app-album-photos',
   templateUrl: './album-photos.component.html',
   styleUrls: ['./album-photos.component.css']
 })
-export class AlbumPhotosComponent {
+export class AlbumPhotosComponent implements OnInit{
+  albumId:any;
+  albumPhotos:any;
 
+  constructor(private myService:AlbumsService, private curRoute:ActivatedRoute){
+    this.albumId = this.curRoute.snapshot.params['albumId'];
+  }
+
+  ngOnInit() {
+    this.myService.getAlbumPhotos(this.albumId).subscribe({
+      next: (data) => this.albumPhotos = data,
+      error: (err) => console.log(err)
+    })
+  }
 }

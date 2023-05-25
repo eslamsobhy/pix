8d247194c8f1bb47344b27a88e6949291284b6c8
asmaatajeldein <asmaatajeldein@gmail.com>
2023-05-25 21:49:38 +0300

    create 'photo-card' component

--- src/app/Components/photo-card/photo-card.component.ts
+++ src/app/Components/photo-card/photo-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo-card',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./photo-card.component.css']
 })
 export class PhotoCardComponent {
+  @Input() photoInfo:any;
 
+  mouseOverStar = false;
+
+  mouseOver() {
+    this.mouseOverStar = true;
+  }
+
+  mouseOut() {
+    this.mouseOverStar = false;
+  }
 }

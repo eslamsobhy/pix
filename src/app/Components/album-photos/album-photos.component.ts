5941f26bfe1f0c31e95a7c587a649546c122e934
asmaatajeldein <asmaatajeldein@gmail.com>
2023-05-26 23:15:24 +0300

    fetch user info and user albums

--- src/app/Components/album-photos/album-photos.component.ts
+++ src/app/Components/album-photos/album-photos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlbumsService } from 'src/app/services/albums.service';
+import { UsersInfoService } from 'src/app/services/users-info.service';
 
 @Component({
   selector: 'app-album-photos',
@@ -8,16 +9,33 @@ import { AlbumsService } from 'src/app/services/albums.service';
   styleUrls: ['./album-photos.component.css']
 })
 export class AlbumPhotosComponent implements OnInit{
+  userId:any;
+  userInfo:any;
   albumId:any;
+  albumInfo:any;
   albumPhotos:any;
 
-  constructor(private myService:AlbumsService, private curRoute:ActivatedRoute){
+  constructor(private usersService:UsersInfoService, private albumsService:AlbumsService, private curRoute:ActivatedRoute){
     this.albumId = this.curRoute.snapshot.params['albumId'];
+    this.userId = this.curRoute.snapshot.params['userId'];
   }
 
   ngOnInit() {
-    this.myService.getAlbumPhotos(this.albumId).subscribe({
+    this.usersService.getUserById(this.userId).subscribe({
+      next: (data) => this.userInfo = data,
+      error: (err) => console.log(err)
+    })
+
+    this.albumsService.getAlbumPhotos(this.albumId).subscribe({
       next: (data) => this.albumPhotos = data,
+      error: (err) => console.log(err)
+    })
+
+    this.albumsService.getUserAlbums(this.userId).subscribe({
+      next: (data) => {
+        let albums: {[index: string]:any} = data;
+        this.albumInfo = albums[this.albumId - 1];
+      },
       error: (err) => console.log(err)
     })
   }

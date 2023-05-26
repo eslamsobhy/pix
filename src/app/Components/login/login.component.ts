165c0e09c393db77215b3d55574cc25aeb437d5f
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-26 17:22:59 +0300

    added validation to the form

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  myValidation = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      // Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@#$%^&+=]).*$'),
+    ]),
+  });
 
+  join() {
+    console.log(this.myValidation.valid);
+  }
 }

232efa864fc3833ea2a08afe0aaf0710c39f3683
asmaatajeldein <asmaatajeldein@gmail.com>
2023-05-27 17:16:36 +0300

    add routing for testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { UserAlbumsComponent } from './components/user-albums/user-albums.component';
 import { AlbumPhotosComponent } from './components/album-photos/album-photos.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 const routes: Routes = [
-  {path: 'users/createNew', component: AddUserComponent },
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersListComponent},
+  {path: 'users/add', component: AddUserComponent },
   {path: 'users/:userId/albums', component: UserAlbumsComponent},
   {path: 'users/:userId/albums/:albumId', component: AlbumPhotosComponent}
 ];
